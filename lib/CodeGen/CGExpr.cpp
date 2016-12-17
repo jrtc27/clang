baa8daa47b9847dcce885a1ea19eecfd91435b46
James Clarke <jrtc27@jrtc27.com>
2016-12-17 15:42:00 +0000

    CGExpr: Reinstate llvm_unreachable seemingly lost in a merge at least a year ago

--- lib/CodeGen/CGExpr.cpp
+++ lib/CodeGen/CGExpr.cpp
@@ -4371,6 +4371,7 @@ RValue CodeGenFunction::convertTempToRValue(Address addr,
   case TEK_Scalar:
     return RValue::get(EmitLoadOfScalar(lvalue, loc));
   }
+  llvm_unreachable("bad evaluation kind");
 }
 
 void CodeGenFunction::SetFPAccuracy(llvm::Value *Val, float Accuracy) {
